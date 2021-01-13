b2858b20009e8316897bb2a1629cafacca2600c9
Srijan Nayak <srijannayak0@gmail.com>
2021-01-13 21:56:04 +0530

    feat: Use babel-loader for js files

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,6 @@
+import { handleSubmit } from "./js/formHandler";
+import { checkForName } from "./js/nameChecker";
+
 alert("this is a test");
+console.log(handleSubmit);
+console.log(checkForName);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
