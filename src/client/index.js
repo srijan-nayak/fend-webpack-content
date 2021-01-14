c6ba26c4d5aad6e9e85b041ed303e180c7edc994
Srijan Nayak <srijannayak0@gmail.com>
2021-01-14 11:54:43 +0530

    fix: Delete alert in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,5 @@
 import { handleSubmit } from "./js/formHandler";
 import { checkForName } from "./js/nameChecker";
 
-alert("this is a test");
 console.log(handleSubmit);
 console.log(checkForName);
