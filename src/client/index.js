8c47d6a7c7e3d419f0748b61d3090774e7f1cea8
Srijan Nayak <srijannayak0@gmail.com>
2021-01-14 20:11:26 +0530

    feat: Load styles from Sass

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,9 @@
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+
 import { handleSubmit } from "./js/formHandler";
 import { checkForName } from "./js/nameChecker";
 
